de7d4825ee0b8cd0d8b4a03359d074e87244d82c
Daniel <aklogdaniel@gmail.com>
2023-04-02 16:33:53 +0300

    upload files button working

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 const mysql = require("mysql2");
+var formidable = require("formidable");
+const path = require("path");
+const { log, error } = require("console");
+const uploadFolder = path.join(__dirname, "uploads");
 
 const pool = mysql.createPool({
 	host: "localhost",
@@ -26,6 +30,26 @@ app.get("/files", function (req, res) {
 	pool.query("SELECT * FROM files", function (err, rows, fields) {
 		if (err) console.error(err);
 		res.json(rows);
+	});
+});
+
+app.post("/files", function (req, res) {
+	const form = formidable();
+	form.maxFileSize = 10 * 1024 * 1024;
+	form.uploadDir = uploadFolder;
+	form.parse(req, (err, fields, { file }) => {
+		if (err) {
+			error(err);
+		}
+		pool.query(
+			"INSERT INTO files (file_name, file_size) VALUES (?,?)",
+			[file.originalFilename, file.size],
+			function (err, rows, fields) {
+				if (err) console.error(err);
+				// res.json(rows);
+			}
+		);
+		res.json({ files: file, fields });
 	});
 });
 
