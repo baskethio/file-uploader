11cd3cd8ed683db049efde46073b0ccc14149ae2
Daniel <aklogdaniel@gmail.com>
2023-04-02 15:36:21 +0300

    file upload button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import "./App.css";
+import { FileInput, Button, Center } from "@mantine/core";
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:5000";
 
 function App() {
-	const [file, setFile] = useState();
+	const [file, setFile] = useState<File | null>();
+
+	const uploadFile = () => {
+		if (file) {
+			const formData = new FormData();
+			formData.append("file", file);
+			axios
+				.post(`${SERVER_URL}/files`, formData)
+				.then((response) => {})
+				.catch((error) => {});
+		}
+	};
 
 	return (
 		<div>
 			<h3>File upload</h3>
+			<Center>
+				<FileInput placeholder="Pick file" onChange={setFile} />
+				<Button onClick={uploadFile}>Upload</Button>
+			</Center>
 		</div>
 	);
 }
