f964eeda9c16a221bbf175b5f9fa4d767d8a8b45
Daniel <aklogdaniel@gmail.com>
2023-04-02 15:24:04 +0300

    starting to work on client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [file, setFile] = useState();
+
+	return (
+		<div>
+			<h3>File upload</h3>
+		</div>
+	);
+}
+
+export default App;
